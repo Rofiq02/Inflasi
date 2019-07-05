54e9941484be8fcdba9bb7300a8e8dd45afb0bb6
Rofiq02 <abdulrafiqyusufa84@gmail.com>
2019-07-05 14:20:07 +0700

    inflasi

--- app/Http/Controllers/MobileControl.php
+++ app/Http/Controllers/MobileControl.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use Session; 
+
+
+
 
 class MobileControl extends Controller
 {
@@ -50,7 +56,7 @@ class MobileControl extends Controller
 			header ("Access-Control-Allow-Origin: *");
 			header('Access-Control-Allow-Headers: Authorization, Content-Type');
 
-			$kalender = DB::Select('select tb_tahun_kalender.besar_inflasi,tb_bulan.nama_bulan from tb_tahun_kalender,tb_bulan where tb_tahun_kalender.kd_bulan = tb_bulan.kd_bulan and tahun = (curdate()) limit 5');
+			$kalender = DB::Select('select tb_tahun_kalender.besar_inflasi,tb_bulan.nama_bulan from tb_tahun_kalender,tb_bulan where tb_tahun_kalender.kd_bulan = tb_bulan.kd_bulan and tahun = (curdate()) limit 4');
 
 			echo json_encode($kalender);
 		}
@@ -100,59 +106,150 @@ class MobileControl extends Controller
 
 			echo json_encode($kategori);
 		}
+
+		function get_inflasi_kemarin(){
+			header ("Access-Control-Allow-Origin: *");
+			header('Access-Control-Allow-Headers: Authorization, Content-Type');
+
+			$inflasikemarin = DB::Select('select tb_inflasi_bulanan.besar_inflasi,tb_bulan.nama_bulan,tb_inflasi_bulanan.tahun from tb_inflasi_bulanan,tb_bulan where tb_bulan.kd_bulan = tb_inflasi_bulanan.kd_bulan order by besar_inflasi desc limit 1');
+
+			echo json_encode($inflasikemarin);
+		}
+
+		function get_kalender_terkini(){
+			header ("Access-Control-Allow-Origin: *");
+			header('Access-Control-Allow-Headers: Authorization, Content-Type');
+
+			$kalenderterkini = DB::Select('select besar_inflasi from tb_tahun_kalender where tahun=year(curdate()) and kd_bulan=month(curdate())');
+
+			echo json_encode($kalenderterkini);
+		}
+
+		function get_kategori_sekarang(){
+			header ("Access-Control-Allow-Origin: *");
+			header('Access-Control-Allow-Headers: Authorization, Content-Type');
+
+			$kategorisekarang = DB::Select('select tb_kategori.nama_kategori, tb_inflasi_kategori.nilai_inflasi from tb_kategori,tb_inflasi_kategori where tb_kategori.kd_kategori = tb_inflasi_kategori.kd_kategori and tahun=year(curdate()) and kd_bulan=month(curdate())');
+
+			echo json_encode($kategorisekarang);
+		}
+
+		function get_yoy_limit(){
+			header ("Access-Control-Allow-Origin: *");
+			header('Access-Control-Allow-Headers: Authorization, Content-Type');
+			
+			$yoylimit = DB::Select('select tb_bulan.nama_bulan,tb_yoy.tahun,tb_bulan2.nama_bulan,tb_yoy.tahun2,tb_yoy.besar_inflasi from tb_bulan,tb_yoy,tb_bulan2 where tb_bulan.kd_bulan = tb_yoy.kd_bulan and tb_bulan2.id = tb_yoy.id and tahun = year(curdate()) and tb_bulan.nama_bulan <= month(curdate()) limit 4');
+
+			echo json_encode($yoylimit);
+		}
 		
+		function register(Request $req){
+			header('Access-Control-Allow-Origin: *');
+			header('Access-Control-Allow-Headers: Authorization, Content-Type' );
+
+			
+
+			$new = $req->all(); // get data dari ionic
+
+			$token = csrf_token(); 
+			$user = new User([
+				"name"=>$new['name'],
+				"alamat"=>$new['alamat'],
+				"telp"=>$new['telp'],
+				"email"=>$new['email'],
+				"password"=>Hash::make($new['password']),
+				"level"=>3,
+				"remember_token"=>$token
+			]);
+			
+			// Nilai balik
+			if($user->save()){
+				echo json_encode(["type"=>"success","msg"=>$new]);			
+			} else {
+				echo json_encode(["type"=>"error","msg"=>"Data Gagal Disimpan ! "]);
+			}
+		}
+
+		function login(Request $req){
+			header('Access-Control-Allow-Origin: *');
+			header('Access-Control-Allow-Headers: Authorization, Content-Type' );
+	
+			$login = $req->all();
+	
+			// Cek Email
+			$ceklogin = DB::table('users')->where('email',$login['email'])->first();
+			if($ceklogin){
+				if(Hash::check($login["password"],$ceklogin->password)){
+					$profile = DB::select('SELECT id,name,alamat,jk,telp,pekerjaan,instansi,email,level,avatar from users WHERE users.email="'.$login['email'].'"');
+					echo json_encode(["type"=>"success","profile"=>$profile[0]]);
+				} else {
+					echo json_encode(["type"=>"error pass","msg"=>"Password Invalid !"]);
+				}
+			} else {
+				echo json_encode(["type"=>"error email","msg"=>"Email Invalid !"]);
+			}
+	
+		}
 
     function save_member(Request $req){
 		header('Access-Control-Allow-Origin: *');
 		header('Access-Control-Allow-Headers: Authorization, Content-Type' );
+		
 		try {
-			$data = $req->json()->all();
-			if($data['users']==null){
-			
-				$newid = DB::select('SHOW TABLE STATUS LIKE "tb_anggota"');
-				$nousers = "A".sprintf('%04d',$newid[0]->Auto_increment).date("mY");
 
+			$req = $req->json()->all();
+			if($req['id']==null){
+			
+				// $newid = DB::select('SHOW TABLE STATUS LIKE "tb_anggota"');
+				// $nousers = "A".sprintf('%04d',$newid[0]->Auto_increment).date("mY");
+				$rek = "add";
 				// Simpan Ke Tabel Anggota
 				$res = DB::table('users')->insert([
-                    'name' => $data['name'],
-                    'alamat' => $data['alamat'],
-                    'jk' => $data['jk'],
-										'telp' => $data['telp'],
-										'pekerjaan' => $data['pekerjaan'],
-										'instansi' => $data['instansi'],
-                    'email' => $data['email'],
-			    	'status'=>1
+                    'name' => $req['name'],
+                    'alamat' => $req['alamat'],
+                    'jk' => $req['jk'],
+					'telp' => $req['telp'],
+					'pekerjaan' => $req['pekerjaan'],
+					'instansi' => $req['instansi'],
+                    'email' => $req['email'],
+			    	'level'=>1
 				]);		
 
 			} else {
-				$res = DB::table('users')->where("email",$data['email'])->update([
-					'name' => $data['name'],
-                    'alamat' => $data['alamat'],
-                    'jk' => $data['jk'],
-										'telp' => $data['telp'], 
-										'pekerjaan' => $data['pekerjaan'],
-										'instansi' => $data['instansi'],
+				$rek ="edit";
+				$res = DB::table('users')->where("email",$req['email'])->update([
+					'name' => $req['name'],
+                    'alamat' => $req['alamat'],
+                    'jk' => $req['jk'],
+					'telp' => $req['telp'], 
+					'pekerjaan' => $req['pekerjaan'],
+					'instansi' => $req['instansi'],
+					'email' => $req['email'],
 				]);	
 			}
 
 			$success = 1;
 		} catch (\Exception $e){
 			$success = 0;
+			// $messg = $e;
+		    // echo json_encode(["type"=>"error ","message"=>$req,"rek"=>$rek]);
 		}
 		
 		if($success == 1){
-			$profile = DB::select('SELECT * from users WHERE users.email="'.$data['email'].'"');
+			// $profile = DB::select('SELECT * from users WHERE users.email="'.$req['email'].'"');
+			// dd($profile); 
+			// // Save Base64 to Im
+			// $image = $req['avatar'];  // your base64 encoded
+			// $image = str_replace('data:image/jpeg;base64,', '', $image);
+			// $image = str_replace(' ', '+', $image);
+			// $imageName = str_random(10).'.'.'png';
+			// \File::put(public_path(). '/img' . $imageName, base64_decode($image));
 
-			// Save Base64 to Im
-			$image = $data['foto'];  // your base64 encoded
-			$image = str_replace('data:image/jpeg;base64,', '', $image);
-			$image = str_replace(' ', '+', $image);
-			$imageName = str_random(10).'.'.'png';
-			\File::put(public_path(). '/img' . $imageName, base64_decode($image));
+			
 
-			echo json_encode(["type"=>"success","profile"=>$profile[0],"g"=>$data['foto']]);
+			echo json_encode(["type"=>"success","profile"=>$req]);
 		} else {
-			echo json_encode(["type"=>"error","mess"=>"Error disimpan !"]);
+		    echo json_encode(["type"=>"error ","message"=>$messg]);
 		}		
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,6 @@ Route::post('/mobile/save_member','MobileControl@save_member');
 Route::get('/mobile/get_inflasi','MobileControl@get_inflasi');
 Route::get('/mobile/get_bulanan/{tahuna?}','MobileControl@get_bulanan');
 Route::get('/mobile/get_kalender/{tahun?}','MobileControl@get_kalender');
-Route::get('/mobile/get_kalenderkmrn','MobileControl@get_kalenderkmrn');
-Route::get('/mobile/get_kalenderkmrnlgi','MobileControl@get_kalenderkmrnlgi');
 Route::get('/mobile/limit_kalender','MobileControl@limit_kalender');
 Route::get('/mobile/get_inflasi_kategori','MobileControl@get_inflasi_kategori');
 Route::get('/mobile/get_inflasi_item','MobileControl@get_inflasi_item');
@@ -32,5 +30,12 @@ Route::get('/mobile/get_inflasi_komoditas/{kd_bulan?}/{tahun?}','MobileControl@g
 Route::get('/mobile/get_inflasi_yoy/{tahun?}','MobileControl@get_inflasi_yoy');
 Route::get('/mobile/get_inflasi_terkini','MobileControl@get_inflasi_terkini');
 Route::get('/mobile/get_kategori/{tahun?}','MobileControl@get_kategori');
+Route::get('/mobile/get_inflasi_kemarin','MobileControl@get_inflasi_kemarin');
+Route::get('/mobile/get_kalender_terkini','MobileControl@get_kalender_terkini');
+Route::get('/mobile/get_kategori_sekarang','MobileControl@get_kategori_sekarang');
+Route::get('/mobile/get_yoy_limit','MobileControl@get_yoy_limit');
+Route::post('mobile/register','MobileControl@register');
+Route::post('mobile/login','MobileControl@login');
+
 
 
